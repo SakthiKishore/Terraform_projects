c80baf1322a751bb4bfd6964891e7332adfadcf9
TheCoder <31217771+SakthiKishore@users.noreply.github.com>
2021-01-07 12:59:49 +0530

    Create backend.tf

---
+++ Deploy_AKS/backend.tf
@@ -0,0 +1,9 @@
+  
+terraform {
+  // backend "azurerm" {
+  //   resource_group_name  = "tf_state"
+  //   storage_account_name = "tfstate019"
+  //   container_name       = "tfstate"
+  //   key                  = "terraform.tfstate"
+  // }
+}
